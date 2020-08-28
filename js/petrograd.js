fb1b1deacd28538781a61c59ccdbdd6be4ed1ac2
Jonas Holbech <jh@jonasholbech.dk>
2020-08-28 12:10:58 +0200

    products cloned, initial values set

--- js/petrograd.js
+++ js/petrograd.js
@@ -5,19 +5,31 @@ fetch("https://kea-alt-del.dk/t5/api/productlist")
     return response.json();
   })
   .then(function (data) {
-    console.log(data)
+
+    dataReceived(data);
   })
 
-//console.log(data)
-//loop through products
+function dataReceived(products) {
+  //loop through products
+  products.forEach(showProduct)
+}
 
-//finding the template
+//executed once for each product
+function showProduct(myProduct) {
+  console.log(myProduct)
+  //finding the template
+  const temp = document.querySelector("#productTemplate").content;
+  //clone the template
+  const myCopy = temp.cloneNode(true);
+  //fill out the template
+  myCopy.querySelector(".data_name").textContent = myProduct.name;
+  //append
+  const parentElem = document.querySelector("section#starter");
+  parentElem.appendChild(myCopy)
+}
 
-//clone the template
 
-//fill out the template
 
-//append
 /*
 const name1 = "Jonas";
 
